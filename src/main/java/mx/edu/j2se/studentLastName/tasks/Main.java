d03dda808c7553de2dc01cd70a4645e9a61c2de5
Abraham <acl970804@gmail.com>
2021-11-03 18:16:20 -0600

    Practica 0 Initial commit

---
+++ src/main/java/mx/edu/j2se/studentLastName/tasks/Main.java
@@ -0,0 +1,10 @@
+package mx.edu.j2se.camarillo.tasks;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo!");
+		System.out.println("Adios Mundo");
+	}
+	
+}
